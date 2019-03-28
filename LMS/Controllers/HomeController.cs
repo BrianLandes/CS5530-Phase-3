96244b10452228762932d7fc67c7f0c6f9202746
Brian Landes <the.brian.landes@gmail.com>
2019-03-28 12:49:31 -0600

    Format controllers

--- LMS/Controllers/HomeController.cs
+++ LMS/Controllers/HomeController.cs
@@ -7,49 +7,40 @@ using Microsoft.AspNetCore.Mvc;
 using LMS.Models;
 using Microsoft.AspNetCore.Authorization;
 
-namespace LMS.Controllers
-{
-  public class HomeController : CommonController
-  {
+namespace LMS.Controllers {
+	public class HomeController : CommonController {
 
-    public IActionResult Index()
-    {
-      if(User.IsInRole("Student"))
-      {
-        return Redirect("/Student/Index");
-      }
-      if(User.IsInRole("Professor"))
-      {
-        return Redirect("/Professor/Index");
-      }
-      if(User.IsInRole("Administrator"))
-      {
-        return Redirect("/Administrator/Index");
-      }
+		public IActionResult Index() {
+			if (User.IsInRole("Student")) {
+				return Redirect("/Student/Index");
+			}
+			if (User.IsInRole("Professor")) {
+				return Redirect("/Professor/Index");
+			}
+			if (User.IsInRole("Administrator")) {
+				return Redirect("/Administrator/Index");
+			}
 
-      return View();
-    }
+			return View();
+		}
 
 
-    public IActionResult About()
-    {
-      ViewData["Message"] = "Your application description page.";
+		public IActionResult About() {
+			ViewData["Message"] = "Your application description page.";
 
-      return View();
-    }
+			return View();
+		}
 
-    [Authorize(Roles = "Administrator")]
-    public IActionResult Contact()
-    {
-      ViewData["Message"] = "Your contact page.";
+		[Authorize(Roles = "Administrator")]
+		public IActionResult Contact() {
+			ViewData["Message"] = "Your contact page.";
 
-      return View();
-    }
+			return View();
+		}
 
 
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-  }
+		public IActionResult Error() {
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
