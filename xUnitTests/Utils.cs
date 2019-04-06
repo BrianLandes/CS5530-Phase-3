4fc13dc9043ddc2a3690cb927f6ee3b54dc40b90
Brian Landes <the.brian.landes@gmail.com>
2019-04-06 10:46:09 -0600

    Setup Testing Framework

---
+++ xUnitTests/Utils.cs
@@ -0,0 +1,19 @@
+﻿using LMS.Models.LMSModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace xUnitTests {
+	public static class Utils {
+
+		public static Team41LMSContext MakeMockDatabase() {
+			var options = new DbContextOptionsBuilder<Team41LMSContext>();
+			// had to run:
+			// Install-Package Microsoft.EntityFrameworkCore.InMemory
+			// in the Package Manager Console to get this command
+			options.UseInMemoryDatabase();
+
+			Team41LMSContext db = new Team41LMSContext(options.Options);
+			return db;
+
+		}
+	}
+}
